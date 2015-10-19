19532fe63a974435f43f592c0942491035e8cdcf
robertoyubero <robertoyubero@gmail.com>
2015-10-19 18:55:11 +0200

    pasado pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,17 +9,15 @@ from xml.sax import make_parser
 
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
 
         self.tags = []
         self.dic = {
-        "root-layout": ['width', 'height', 'background-color'],
-        "region": ['id', 'top', 'bottom', 'left', 'right'],
-        "img": ['src', 'region', 'begin', 'dur'],
-        "audio": ['src', 'begin', 'dur'],
-        "textstream": ['src', 'region']
-        }
-
+            "root-layout": ['width', 'height', 'background-color'],
+            "region": ['id', 'top', 'bottom', 'left', 'right'],
+            "img": ['src', 'region', 'begin', 'dur'],
+            "audio": ['src', 'begin', 'dur'],
+            "textstream": ['src', 'region']}
 
     def startElement(self, name, attrs):
         """
